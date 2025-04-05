27d1c5736279b53923c49e8ff498b8862c6c771b
Ernests <elejins@inbox.lv>
2025-04-05 17:19:15 +0300

    Exercises done itll sixth

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
 const reverseString = function() {
+    const string = '';
+    let array = [];
 
+    for(let i = string.length - 1; i>=0; i--){
+        array.push(string.charAt(i))
+    }
+
+    let newString = array.join("");
+
+    return newString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...argument) {
+    for(let i = 0; i < arr.length; i++){
+
+        for(let y = 0; y < argument.length; y++){
+
+            if(arr[i] === argument[y]){
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+        
+    };
+    return arr;
 };
+
+removeFromArray([1, 2, 3], "1", 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
